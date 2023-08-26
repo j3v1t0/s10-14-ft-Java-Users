fd43d987885b0751994efc0676c517a89935fccf
Alfonso Almonte <alfonsoalmonte@gmail.com>
2023-08-25 23:18:08 -0400

    Updatiing configuration

--- src/main/java/s1014ftjavaangular/userservice/UserServiceApplication.java
+++ src/main/java/s1014ftjavaangular/userservice/UserServiceApplication.java
@@ -17,12 +17,12 @@ public class UserServiceApplication {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
-	@Bean
+/*	@Bean
 	public EurekaClientConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
 		var config = new EurekaClientConfigBean();
 		config.setServiceUrl(Map.of("defaultZone", "https://s10-14-ft-eurekaserver.azurewebsites.net/eureka"));
 		//config.setServiceUrl(Map.of("defaultZone", "http://localhost:8761/eureka"));
 
 		return config;
-	}
+	}*/
 }
